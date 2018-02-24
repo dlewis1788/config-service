4a0c86e5fce69462c1845e38e3f6863c51844dca
Derek Lewis <derek_lewis@projectivesoftware.com>
2018-02-24 09:52:16 -0600

    Upgrade to Spring Boot 2.0.0.RC2

--- src/main/java/com/projectivesoftware/config/ConfigServiceApplication.java
+++ src/main/java/com/projectivesoftware/config/ConfigServiceApplication.java
@@ -8,7 +8,7 @@ package com.projectivesoftware.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
